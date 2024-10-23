7b7e43bd83e1d023cd40f2824eaa5770c1a91616
SB2157 <125543017+SB2157@users.noreply.github.com>
2024-10-23 23:05:29 +0530

    Update Gradecal.java

--- Core_Java/Gradecal.java
+++ Core_Java/Gradecal.java
@@ -1,72 +1,28 @@
+package Core_Java;
+
 import java.util.Scanner;
 
 public class Gradecal {
-    public static void main(String[] args) {
-        //create object / Instance
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        // Enter total number of subjects to calculate their marks
-        System.out.println("Enter total number of subjects: ");
-        int numSubjects = scan.nextInt();
+		System.out.print("Enter student's grade (0-100): ");
+		int grade = sc.nextInt();
 
-        int totalMarks = 0;
+		if (grade >= 90) {
+			System.out.println("First grade:" + grade);
+		} else if (grade >= 80) {
+			System.out.println("Second grade:" + grade);
+		} else if (grade >= 70) {
+			System.out.println("Third grade:" + grade);
+		} else if (grade >= 60) {
+			System.out.println("Forth grade:" + grade);
+		} else if (grade >= 0) {
+			System.out.println("Fail:" + grade);
+		} else {
+			System.out.println("Invalid Grade");
+		}
 
-        for (int i = 1; i <= numSubjects; i++) {
-            System.out.println("Enter marks for subject " + i + "(out of 100): ");
-            int marks = scan.nextInt();
+	}
 
-            // INPUT VALIDATION
-            while (marks < 0 || marks > 100) {
-                System.out.println("Invalid marks. Please enter marks between 0 and 100.");
-                System.out.println("Enter marks for subject " + i + "out of 100): ");
-                marks = scan.nextInt();
-            }
-            totalMarks += marks;
-        }
-        System.out.println("Student Result: ");
-        System.out.println("Total marks obtained in all subjects: " + totalMarks);
-
-        // Calculate Average Percentage: Divide the 'total marks' by the 'total number
-        // of subjects' to get the average percentage.
-
-        int AveragePercentage = totalMarks / numSubjects;
-
-        System.out.println("Average Percentage: " + AveragePercentage);
-
-        // Grade Calculation: Assign grades based on the average percentage achieved.
-
-        if (AveragePercentage >= 90) {
-            System.out.println("Your grade: A+");
-        }
-
-        else if (AveragePercentage >= 80) {
-            System.out.println("Your grade: B+");
-        }
-
-        else if (AveragePercentage >= 70) {
-            System.out.println("Your grade: B");
-        }
-
-        else if (AveragePercentage >= 60) {
-            System.out.println("Your grade: C+");
-        }
-
-        else if (AveragePercentage >= 50) {
-            System.out.println("Your grade: C");
-        }
-
-        else if (AveragePercentage >= 40) {
-            System.out.println("Your grade: D+");
-        }
-
-        else if (AveragePercentage >= 30) {
-            System.out.println("Your grade: D");
-        }
-
-        else {
-            System.out.println("Your grade is: E");
-        }
-
-        scan.close();
-    }
 }
